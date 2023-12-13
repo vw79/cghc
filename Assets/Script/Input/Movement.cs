61d345308ba0ea1229fb86323cdefa61dc1ce943
Ronfreddy <eonngyiyong@gmail.com>
2023-12-13 11:18:00 +0800

    fixed gravity bug with raycast
    now trying better solution using boxcast

--- Assets/Script/Input/Movement.cs
+++ Assets/Script/Input/Movement.cs
@@ -10,13 +10,23 @@ public class Movement : MonoBehaviour
     private Vector3 originalScale;
     private float moveVectorX = 0;
     private float moveVectorY = 0;
-    public float horizontalMoveSpeed = 5f;
+    private float horizontalMoveSpeed = 5f;
     private float acceleration = 0;
 
-    private bool onGround = false;
+    public bool onGround = false;
 
     private int jumpCount = 0;
     private int jumpLimit = 2;
+
+    private Vector2 _boundsTopLeft;
+    private Vector2 _boundsTopRight;
+    private Vector2 _boundsBottomLeft;
+    private Vector2 _boundsBottomRight;
+
+    private float _boundsWidth;
+    private float _boundsHeight;
+
+
 
     private void Awake()
     {
@@ -26,18 +36,29 @@ public class Movement : MonoBehaviour
 
     private void Update()
     {
+
         HorizontalMovement();
         VerticalMovement();
 
         //Apply movement
         rb.velocity = new Vector2(moveVectorX * horizontalMoveSpeed, moveVectorY);
 
-        animator.SetFloat("Speed", Mathf.Abs(moveVectorX));
 
         if (moveVectorX > 0)
             transform.localScale = new Vector3(originalScale.x, originalScale.y, originalScale.z);
         else if (moveVectorX < 0)
             transform.localScale = new Vector3(-originalScale.x, originalScale.y, originalScale.z);
+
+
+
+        animator.SetFloat("Speed", Mathf.Abs(moveVectorX));
+
+
+        // Gravity detection
+        SetRayOrigins();
+        DetectGravity();
+
+
     }
 
     private void HorizontalMovement()
@@ -81,7 +102,7 @@ public class Movement : MonoBehaviour
             if(jumpCount < jumpLimit)
             {
                 onGround = false;
-                moveVectorY = 20f;  //original 14f
+                moveVectorY = 30f;  //original 14f
                 jumpCount++;
 
                 animator.Play("jump");
@@ -89,12 +110,72 @@ public class Movement : MonoBehaviour
         }
     }
 
+    /*
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.layer == LayerMask.NameToLayer("Floor"))
         {
             onGround = true;
             jumpCount = 0;
+        }
+    }
+    */
+
+    private void SetRayOrigins()
+    {
+        Bounds playerBounds = this.GetComponent<BoxCollider2D>().bounds;
+
+        _boundsBottomLeft = new Vector2(playerBounds.min.x, playerBounds.min.y);
+        _boundsBottomRight = new Vector2(playerBounds.max.x, playerBounds.min.y);
+        _boundsTopLeft = new Vector2(playerBounds.min.x, playerBounds.max.y);
+        _boundsTopRight = new Vector2(playerBounds.max.x, playerBounds.max.y);
+
+        _boundsHeight = Vector2.Distance(_boundsBottomLeft, _boundsTopLeft);
+        _boundsWidth = Vector2.Distance(_boundsBottomLeft, _boundsBottomRight);
+    }
+
+    private void DetectGravity()
+    {
+        // Calculate ray origin
+        Vector2 leftOrigin = ((_boundsBottomLeft + _boundsTopLeft) / 2f) + new Vector2(0.02f,0);
+        Vector2 rightOrigin = ((_boundsBottomRight + _boundsTopRight) / 2f) - new Vector2(0.02f, 0);
+        Vector2 downOrigin = (_boundsBottomLeft + _boundsBottomRight) / 2;
+
+        float rayLength = _boundsHeight / 2f;
+        if(moveVectorY < 0)
+        {
+            rayLength = _boundsHeight / 2f - moveVectorY / 100f;
+        }
+
+        RaycastHit2D[] hits = new RaycastHit2D[4];
+        for (int i = 0; i < 4; i++)
+        {
+            Vector2 rayOrigin = Vector2.Lerp(leftOrigin, rightOrigin, (float)i / (float)(4 - 1));
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, -transform.up, rayLength, LayerMask.GetMask("Floor"));
+            hits[i] = hit;
+            Debug.DrawRay(rayOrigin, -transform.up * rayLength, Color.green);
+        }
+        RaycastHit2D hit2 = Physics2D.BoxCast(downOrigin, new Vector2(_boundsWidth, 0.1f), 0, Vector2.down, rayLength, LayerMask.GetMask("Floor"));
+        Debug.Log(hit2.collider);
+
+        // Check if any of the rays hit the ground
+        bool rayHit = false;
+        foreach (RaycastHit2D hit in hits)
+        {
+            if (hit)
+            {
+                rayHit = true;
+            }
+        }
+
+        if(rayHit)
+        {
+            onGround = true;
+            jumpCount = 0;
+        }
+        else
+        {
+            onGround = false;
         }
     }
 }
