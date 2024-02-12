88696fea1c8ad1953fef5d2f322ea60cb680f312
vw79 <wshong8282@gmail.com>
2024-02-13 06:03:00 +0800

    Cinematic Bar & Cam Zoom

---
+++ Assets/Scripts/Attack/PlayerAttack.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public bool isAttacking = false;
+
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayer;
+
+    private Animator animator;
+    private PlayerMovement playerMovement;
+    private PlayerHealthSystem playerHealth;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
+        playerHealth = GetComponent<PlayerHealthSystem>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {   
+            
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        if(playerHealth.isDead) return;
+        playerMovement.RB.velocity = new Vector2(0, playerMovement.RB.velocity.y);
+        playerMovement.enabled = false;
+        animator.Play("Attack");
+        StartCoroutine(WaitAnim());
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit " + enemy.name);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+    IEnumerator WaitAnim()
+    {
+        yield return new WaitForSeconds(0.6f);
+        playerMovement.enabled = true;
+    }
+}
---
+++ Assets/Scripts/Camera/CinematicBars.cs
@@ -0,0 +1,149 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Cinemachine;
+
+public class CinematicBars : MonoBehaviour
+{
+    #region Varaibles
+    #region CinematicBars
+    private RectTransform topBar, bottomBar;
+    private float changeSizeAmount;
+    private float targetSize;
+    private bool isActive;
+    #endregion
+
+    #region Cinemachine Zoom
+    private Transform playerTransform;
+    public CinemachineVirtualCamera virtualCamera; 
+    public float zoomInSize = 5f; 
+    public float zoomOutSize = 10f; 
+    private float targetOrthoSize; 
+    private float zoomChangeSpeed;
+    private float originalScreenX;
+    private float originalScreenY;
+    #endregion
+    #endregion
+
+    private void Awake()
+    {
+        GameObject topBarGO = new GameObject("topBar", typeof(Image));
+        topBarGO.transform.SetParent(transform,false);
+        topBarGO.GetComponent<Image>().color = Color.black;
+        topBar = topBarGO.GetComponent<RectTransform>();
+        topBar.anchorMin = new Vector2(0, 1);
+        topBar.anchorMax = new Vector2(1, 1);
+        topBar.sizeDelta = new Vector2(0, 0);
+
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        GameObject btmBarGO = new GameObject("btmBar", typeof(Image));
+        btmBarGO.transform.SetParent(transform, false);
+        btmBarGO.GetComponent<Image>().color = Color.black;
+        bottomBar = btmBarGO.GetComponent<RectTransform>();
+        bottomBar.anchorMin = new Vector2(0, 0);
+        bottomBar.anchorMax = new Vector2(1, 0);
+        bottomBar.sizeDelta = new Vector2(0, 0);
+
+        if (virtualCamera != null)
+        {
+            var framingTransposer = virtualCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
+            if (framingTransposer != null)
+            {
+                // Record the original values
+                originalScreenX = framingTransposer.m_ScreenX;
+                originalScreenY = framingTransposer.m_ScreenY;
+            }
+        }
+    }
+
+    private void Start()
+    {
+        if (virtualCamera != null && playerTransform != null)
+        {
+            virtualCamera.Follow = playerTransform;
+        }
+    }
+    
+    private void Update()
+    {   
+        if (isActive)
+        {
+            Vector2 sizeDelta = topBar.sizeDelta;
+            sizeDelta.y += changeSizeAmount * Time.deltaTime;
+            if (changeSizeAmount > 0)
+            {
+                if (sizeDelta.y >= targetSize)
+                {
+                    sizeDelta.y = targetSize;
+                    isActive = false;
+                }
+            }
+            else
+            {
+                if (sizeDelta.y <= targetSize)
+                {
+                    sizeDelta.y = targetSize;
+                    isActive = false;
+                }                
+            }
+            topBar.sizeDelta = sizeDelta;
+            bottomBar.sizeDelta = sizeDelta;
+        }
+
+        if (isActive && virtualCamera != null)
+        {
+            float targetOrthoSize = targetSize > 0 ? zoomInSize : zoomOutSize;
+            virtualCamera.m_Lens.OrthographicSize = Mathf.MoveTowards(virtualCamera.m_Lens.OrthographicSize, targetOrthoSize, zoomChangeSpeed * Time.deltaTime);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+            { Hide(.3f); }
+
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+            { Show(300, .3f); }
+    }
+
+    public void Show(float targetSize, float time) 
+    { 
+        this.targetSize = targetSize;
+        changeSizeAmount = (targetSize - topBar.sizeDelta.y) / time;
+        isActive = true;
+        SetZoom(zoomInSize, time);
+    }
+
+    public void Hide(float time)
+    {
+        targetSize = 0;
+        changeSizeAmount = -topBar.sizeDelta.y / time;
+        isActive = true;
+        SetZoom(zoomOutSize, time);
+    }
+
+    private void SetZoom(float newSize, float time)
+    {
+        targetOrthoSize = newSize;
+        if (virtualCamera != null)
+        {
+            zoomChangeSpeed = Mathf.Abs(virtualCamera.m_Lens.OrthographicSize - newSize) / time;
+
+            var framingTransposer = virtualCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
+            if (framingTransposer != null)
+            {
+                if (newSize == zoomInSize)
+                {
+                    // Adjust for zoom in
+                    framingTransposer.m_ScreenX = 0.2f;
+                    framingTransposer.m_ScreenY = 0.75f;
+                }
+                else if (newSize == zoomOutSize)
+                {
+                    // Restore the original values when zooming out
+                    framingTransposer.m_ScreenX = originalScreenX;
+                    framingTransposer.m_ScreenY = originalScreenY;
+                }
+            }
+        }
+    }
+}
